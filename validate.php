0f96b6a4a81f65f6c7a882bc94aafcbbe4642bdd
iiiqbd <u.i.adiele@rgu.ac.uk>
2017-02-16 17:14:44 +0000

    edited validate.php

--- validate.php
+++ validate.php
@@ -1,5 +1,5 @@
 <?php
-if (isset($_POST["username"]) || isset($_POST["password"]) {    //Checks if username and password have been passed from login.php
+if (isset($_POST["username"]) || isset($_POST["password"])) {    //Checks if username and password have been passed from login.php
     $username = $_POST["username"];                             //Sets $username variable to the username passed from login.php
     $password = $_POST["password"];                             //Sets $password variable to the password passed from login.php
     header("location: loggedin.php");                           //Sends the user to the "logged in" page. ***TEMPORARY LINK***
