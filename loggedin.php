4f555d8376b227864f2d5d0dbb2b8059ed01b6fd
Wakka121 <1615091@rgu.ac.uk>
2017-02-16 16:56:33 +0000

    Testing for successful login

---
+++ loggedin.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Congrats</title>
+</head>
+<body>
+    <form method="get" action="logout.php"
+    <p>Congratulations you have successfully logged in</p>
+    <button type="button">Logout</button>
+</body>
--- validate.php
+++ validate.php
@@ -2,11 +2,8 @@
 if (isset($_POST["username"]) and isset($_POST["password"]) {   //Checks if username and password have been passed from login.php
     $username = $_POST["username"];                             //Sets $username variable to the username passed from login.php
     $password = $_POST["password"];                             //Sets $password variable to the password passed from login.php
+    header("location: loggedin.php");                           //Sends the user to the "logged in" page. ***TEMPORARY LINK***
 } else {
     header("location: login.php");                              //Else it kicks the user back to login.php
-}
-
-function validateLogin($username, $password) {
-    
 }
 ?>
