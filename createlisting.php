788cb86c54d81917e4e02996622a54e834bae9b0
Wakka121 <1615091@rgu.ac.uk>
2017-03-06 14:29:34 +0000

    More sensible formatting

---
+++ createlisting.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>List your Textbook!</title>
+    <link rel="stylesheet" href="assets/unsemantic-grid-responsive-tablet.css">
+    <link rel="stylesheet" href="assets/style.css">
+</head>
+<body>
+<header>
+    <h1>Textbook Exchange Listings</h1>
+    <nav>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Browse</a></li>
+            <li><a href="#">Search</a></li>
+            <li><a href="#">Create Listing</a></li>
+            <li><a href="#">Profile</a></li>
+            <li><a href="#">Logout</a></li>
+        </ul>
+    </nav>
+</header>
+
+<main>
+    <h3>Use this page to create your book listing!</h3>
+    <form action="#" method="post">
+        <div>
+            <input type="text" name="title" placeholder="Book Title" required><br>
+            <input type="text" name="author" placeholder="Author" required><br>
+            <input type="text" name="ISBN" placeholder="ISBN Number"><br>
+            <input type="text" name="price" placeholder="Requested Price" required><br>
+            <button type="submit">Create Listing</button>
+        </div>
+    </form>
+</main>
+</body>
+</html>
