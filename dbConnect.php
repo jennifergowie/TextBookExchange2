e26cb542df823881ffbb33ae1b7dea12c864ff03
Wakka121 <1615091@rgu.ac.uk>
2017-03-10 13:00:14 +0000

    Updated listings page to reflect new database table format

--- dbConnect.php
+++ dbConnect.php
@@ -2,7 +2,7 @@
 
 /*Connection string information*/
 $connectstr_dbhost = '';
-$connectstr_dbname = '';
+$connectstr_dbname = 'textbookexchange';
 $connectstr_dbusername = '';
 $connectstr_dbpassword = '';
 
@@ -13,7 +13,7 @@ continue;
 
 /*Security Voodoo*/
 $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
-$connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
+//$connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
 $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
 $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
 }
